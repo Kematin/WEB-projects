e9db653b8a12f06c341498869d14b92421044f5d
kematin <kematin@inbox.ru>
2022-08-19 13:22:44 +0500

    Currency Calculate: create forms, add fonts, create markup for html

---
+++ Curency_Calculate/Curency_Calculate/calculate/forms.py
@@ -0,0 +1,13 @@
+from django.forms import Form, FloatField, ChoiceField, NumberInput
+
+currency_suffics = (('usd', 'USD $'), ('eur', 'EUR €'), ('rub', 'RUB ₽'), 
+                ('gbp', 'GBP £'), ('kzt', 'KZT ₸'), ('cny', 'CNY ¥'),
+                ('jpy', 'JPY ¥'))
+class InputValueForm(Form):
+    input_value = FloatField(min_value=0.01,
+            widget=NumberInput(attrs={'placeholder': '0,01'}))
+
+class SelectCurrency(Form):
+    select_1 = ChoiceField(choices=(currency_suffics), label='From')
+    select_2 = ChoiceField(choices=(currency_suffics), label='To')
+
--- Curency_Calculate/Curency_Calculate/calculate/views.py
+++ Curency_Calculate/Curency_Calculate/calculate/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .forms import *
+
 # Create your views here.
+def index(request):
+    return render(request, 'calculate_template/index.html', {
+        'input_value_form': InputValueForm(),
+        'currency_select': SelectCurrency(),
+    })

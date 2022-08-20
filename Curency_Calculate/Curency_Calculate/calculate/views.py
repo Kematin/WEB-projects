c897e44b96f61a8181b6a895ca9bbcc36b1b6eb4
kematin <kematin@inbox.ru>
2022-08-20 13:08:20 +0500

    function: get data from forms

--- Curency_Calculate/Curency_Calculate/calculate/views.py
+++ Curency_Calculate/Curency_Calculate/calculate/views.py
@@ -9,12 +9,26 @@ def error(request, error):
     return render(request, 'calculate_template/error.html')
 
 def index(request):
+    if request.method == 'POST':
+        form = MainCurrencyForm(request.POST)
+        
+        if form.is_valid():
+            value = form.cleaned_data['input_value']
+            c_1 = form.cleaned_data['select_1']
+            c_2 = form.cleaned_data['select_2']
+            total = convert(c_1, c_2, value)
+            return render(request, 'calculate_template/index.html', {
+                'form': MainCurrencyForm(),
+                'total': total,
+            })
+
     return render(request, 'calculate_template/index.html', {
         'form': MainCurrencyForm(),
+        'total': 0,
     })
 
 def convert(currency_1: str, currency_2: str, value: int) -> int:
-    pass
+    return 2
 
 if __name__ == '__main__':
     pass

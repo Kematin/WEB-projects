24ac21dda5173019db0ef49b9316cf9e71bbd10b
kematin <kematin@inbox.ru>
2022-08-20 12:51:22 +0500

    add error page, change forms

--- Curency_Calculate/Curency_Calculate/calculate/views.py
+++ Curency_Calculate/Curency_Calculate/calculate/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
+from currency_converter import CurrencyConverter
+
 from .forms import *
 
 # Create your views here.
+def error(request, error):
+    return render(request, 'calculate_template/error.html')
+
 def index(request):
     return render(request, 'calculate_template/index.html', {
-        'input_value_form': InputValueForm(),
-        'currency_select': SelectCurrency(),
+        'form': MainCurrencyForm(),
     })
+
+def convert(currency_1: str, currency_2: str, value: int) -> int:
+    pass
+
+if __name__ == '__main__':
+    pass
+    # c_1 = 'EUR'
+    # c_2 = 'RUB'
+    # value = 2
+    # answer = convert(c_1, c_2, value)
+    # print(answer)

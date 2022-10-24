417e203949726e83ab7ee198a5f4727cc5bfbacd
kematin <kematin@inbox.ru>
2022-10-24 19:13:01 +0500

    add forms

--- English/English/main_app/views.py
+++ English/English/main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        pass
     return render(request, "index.html")
 
 def handler404(request):

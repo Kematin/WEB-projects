05d405babf899850021e58ab54ae8ee0edd06463
kematin <kematin@inbox.ru>
2022-10-30 14:30:09 +0500

    Adding a dependency of achievements on completed tasks

--- English/English/main_app/main.py
+++ English/English/main_app/main.py
@@ -1,1 +1,32 @@
+class Main:
+    def main(self, answer_1, answer_2, answer_3):
+        templates = self.return_templates()
+        return templates
+
+    def return_templates(self) -> list[dict[str, str]]:
+        info_1 = "Abot achievemnt 1"
+        info_2 = "Abot achievemnt 2"
+        info_3 = "Abot achievemnt 3"
+
+        achievemnt_1 = {
+                "title": "Achievemnt 1", 
+                "info": info_1, 
+                "url": "static/img/img.png"
+                }
+
+        achievemnt_2 = {
+                "title": "Achievemnt 2", 
+                "info": info_2, 
+                "url": "static/img/img.png"
+                }
+
+        achievemnt_3 = {
+                "title": "Achievemnt 3", 
+                "info": info_3, 
+                "url": "static/img/img.png"
+                }
+
+
+        templates = [achievemnt_1, achievemnt_2, achievemnt_3]
+        return templates
 
--- English/English/main_app/views.py
+++ English/English/main_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .forms import TaskForm
+from .main import Main
 
 # Create your views here.
 def index(request):
     context = {
-            "form": TaskForm
+            "form": TaskForm,
+            "templates": []
         }
     if request.method == "POST": 
         form = TaskForm(request.POST)
@@ -12,6 +14,9 @@ def index(request):
             answer_1 = form.cleaned_data["task_1"]
             answer_2 = form.cleaned_data["task_2"]
             answer_3 = form.cleaned_data["task_3"]
+            runner = Main()
+            templates = runner.main(answer_1, answer_2, answer_3)
+            context["templates"] = templates
 
     return render(request, "index.html", context)
 
